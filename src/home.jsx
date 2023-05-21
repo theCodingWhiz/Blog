7854966e5c752669e8860de19c37bfa624b913fa
Muhammad Omar <muhammadomar@Muhammads-MacBook-Pro.local>
2023-05-21 19:32:42 +0100

    updating blogs

--- src/home.jsx
+++ src/home.jsx
@@ -1,17 +1,31 @@
-const Home = () => {
-  const handleClick = () => {
-    console.log("theCodingWhiz", e);
-  };
+import { useState } from "react";
+import BlogList from "./BlogList";
 
-  const handleClickAgain = (name, e) => {
-    console.log("Hello " + name, e.target);
-  };
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("Mo", e)}>Click me again</button>
+      <BlogList blogs={blogs} title="All blogs!" />
     </div>
   );
 };
